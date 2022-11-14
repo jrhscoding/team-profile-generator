660f085a1ff9aec420ed2d6dcff90844f05bf096
joseph <j.hernandez.92189@gmail.com>
2022-11-14 09:02:13 -0800

    put initial inquirer into a function so subsequent functions can be called

--- index.js
+++ index.js
@@ -5,30 +5,36 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'managerName',
-            message: 'Please enter team managers name'
-        },
-        {
-            type: 'input',
-            name: 'managerId',
-            message: 'What is the team managers ID'
-        },
-        {
-            type: 'input',
-            name: 'managerEmail',
-            message: 'What is the team managers email'
-        },
-        {
-            type: 'input',
-            name: 'managerOfficeNumber',
-            message: 'What is your team managers office number'
-        }
-    ])
-    .then((answer) => {
-        const manager = new Manager(answer.managerName, answer.managerId, answer.managerEmail, answer.managerOfficeNumber);
-        console.log(manager);
-    })
+
+const startup = () => {
+    console.log('Please fill out the managers information');
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'managerName',
+                message: 'Please enter team managers name'
+            },
+            {
+                type: 'input',
+                name: 'managerId',
+                message: 'What is the team managers ID'
+            },
+            {
+                type: 'input',
+                name: 'managerEmail',
+                message: 'What is the team managers email'
+            },
+            {
+                type: 'input',
+                name: 'managerOfficeNumber',
+                message: 'What is your team managers office number'
+            }
+        ])
+        .then((answer) => {
+            const manager = new Manager(answer.managerName, answer.managerId, answer.managerEmail, answer.managerOfficeNumber);
+            console.log(manager);
+        })
+};
+
+startup();
