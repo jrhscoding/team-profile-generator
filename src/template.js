c2de26e510c5b8ee7289f13df7e092c36cb93601
joseph <j.hernandez.92189@gmail.com>
2022-11-15 08:41:23 -0800

    added email and id and office id to html manager

--- src/template.js
+++ src/template.js
@@ -2,11 +2,19 @@ const generateCards = team => {
     const managerCard = manager => {
         return `
         <div class="card">
-        <div class="card-header">
-            <h2 class="card-title">${manager.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i> ${manager.getRole()}</h3>
+            <div class="card-header">
+                <h2 class="card-title">${manager.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i> ${manager.getRole()}</h3>
+            </div>
+            <div class="card-body">
+                <ul class ="list-group">
+                    <li class="list-group-item">ID: ${manager.getId()} </li>
+                    <li class="list-group-item">Email: ${manager.getEmail()} </li>
+                    <li class="list-group-item">Office Number: ${manager.getOfficeNumber()} </li>
+                </ul>
+            </div>
         </div>
-        </div>
+
         `
     }
 
